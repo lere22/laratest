5b70b1b6270d2c4c0ee88ff00549a3cbdeaad624
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2021-11-29 15:49:03 +0800

    struktur folder, route & view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Laratest | About</title>
+</head>
+<body>
+    <h1>Hello About!</h1>
+    <p>{{ $name; }}</p>
+    <p><?= $email; ?></p>
+    <img src="{{ asset('images/gambar.jpg') }}" alt="gambar" width="150px">
+</body>
+</html>
