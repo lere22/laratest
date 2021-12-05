a97eeba46e9a8c5e28fa09a71e7f0e4bc496cd05
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2021-12-05 21:02:21 +0800

    database seeder

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,9 +4,10 @@
     <article class="mb-5">
         <h2>{{ $post->title }}</h2>
         {{-- <p>{{ $post->body }}</p> --}}
-        <p>By: Bambang | <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By: <a href="#">{{ $post->user->name }}</a> | <a
+                href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
-    </article>
 
-    <a href="/blog">Back to Posts</a>
+        <p class="mt-4"><a href="/blog">Back to Posts</a></p>
+    </article>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,11 +4,15 @@
     <h1 class="mb-5">Halaman Blog Posts</h1>
 
     @foreach ($posts as $post)
-        <article>
+        <article class="mb-5">
             <h2>
                 <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
             </h2>
+            <p>By: <a href="#">{{ $post->user->name }}</a> | <a
+                    href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
             <p>{{ $post->excerpt }}</p>
+
+            <a href="/posts/{{ $post->slug }}">Read more!</a>
         </article>
     @endforeach
 @endsection
